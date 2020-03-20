c8895bbe8c86596cc35047614dd69afa90e9ee42
gorillasz <i.shvets20011994@gmail.com>
2020-03-20 09:49:53 +0200

    code refactoring

--- index.js
+++ index.js
@@ -1,82 +1,31 @@
-const fetch = require('node-fetch');
-const cheerio = require('cherio');
-const moment = require('moment');
+const moment = require("moment");
+const constants = require("./utils/constants");
+const exchangers = require("./utils/dataMappings");
+process.env.NTBA_FIX_319 = 1; // fix telebot warning message
+const TelegramBot = require("node-telegram-bot-api");
 
-process.env.NTBA_FIX_319 = 1;
-const TelegramBot = require('node-telegram-bot-api');
+const bot = new TelegramBot(constants.token.value, { polling: true });
 
-const TOKEN = '991762238:AAFS97rgMG-fzpWrvXVgmITxX12c5VQbmdU';
-const bot = new TelegramBot(TOKEN, { polling: true });
-const exchangeUrls = {
-  rulya: 'http://rulya-bank.com.ua/',
-  govermentBank:
-    'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json'
-};
-
-const now = `Course of USD on ${moment(new Date()).format('DD/MM/YYYY')}`;
-
-const parseRulya = () =>
-  fetch(exchangeUrls.rulya, { mode: 'cors' }).then(res => res.text());
-const updateRulya = () =>
-  parseRulya()
-    .then(response => {
-      const data = [];
-      const $ = cheerio.load(response);
-      $('#ltbl tr:not(:first-child)').each((i, elem) => {
-        data.push({
-          currency: $(elem)
-            .find('td b')
-            .text()
-            .substring(0, 3),
-          buy: Number(
-            $(elem)
-              .find('td:nth-child(2)')
-              .text()
-          ),
-          sell: Number(
-            $(elem)
-              .find('td:nth-child(3)')
-              .text()
-          )
-        });
-      });
-      return data[0];
-    })
-    .catch(err => {
-      throw new Error(err);
-    });
-
-const parseGovbank = () =>
-  fetch(exchangeUrls.govermentBank, { mode: 'cors' }).then(res => res.text());
-const updateGovbank = () =>
-  parseGovbank()
-    .then(response => {
-      const data = [];
-      const findUsd = JSON.parse(response).find(cur => cur.cc === 'USD');
-      data.push(findUsd);
-      return data[0];
-    })
-    .catch(err => {
-      throw new Error(err);
-    });
-
-const updateAllData = async () => {
+async function dataInit() {
   return {
-    rulya: await updateRulya(),
-    nbu: await updateGovbank()
+    rulya: await exchangers.extractRulyaData(),
+    nbu: await exchangers.extractGovbankData()
   };
-};
-
-async function init() {
-  return await updateAllData();
 }
 
-bot.on('message', msg => {
-  init().then(res => {
-    const formatedData = `${now}\nRulya: ${res.rulya.buy}/${res.rulya.sell}\nNBU: ${res.nbu.rate}\n=^.^=`;
-    const {
-      chat: { id }
-    } = msg;
-    bot.sendMessage(id, formatedData);
-  });
+bot.on("message", msg => {
+  const {
+    chat: { id }
+  } = msg;
+  if (msg.text === "/curex" || msg.text === "/curex@ur_curex_bot") {
+    dataInit()
+    .then(res => {
+      const now = `Course of USD on ${moment(new Date()).format("DD/MM/YYYY")}`;
+      const formatedData = `${now}\n${res.rulya}\n${res.nbu}\n=^.^=`;
+      bot.sendMessage(id, formatedData);
+    })
+    .catch(err => {
+      console.error(`Error on initialization data: ${err}`);
+    });
+  }
 });
---
+++ utils/dataMappings.js
@@ -0,0 +1,67 @@
+const cheerio = require("cherio")
+const dataParsers = require("./dataParsers")
+
+exports.extractRulyaData = () =>
+  dataParsers
+    .parseRulya()
+    .then(res => {
+      const data = []
+      const $ = cheerio.load(res)
+      $("#ltblBAK tr:not(:first-child)").each((i, elem) => {
+        data.push({
+          currency: $(elem)
+            .find("td b")
+            .text()
+            .substring(0, 3),
+          buy: Number(
+            $(elem)
+              .find("td:nth-child(2)")
+              .text()
+          ),
+          sell: Number(
+            $(elem)
+              .find("td:nth-child(3)")
+              .text()
+          )
+        })
+      })
+      const usdCourse =
+        data.length > 0 && data[0].currency === "USD"
+          ? `Rulya: ${data[0].buy}/${data[0].sell}`
+          : "Rate of currency of Rulya exchangers is temporarily unavailable"
+      return usdCourse
+    })
+    .catch(err => {
+      console.error(`Error parsing Rulya data: ${err}`)
+    })
+
+exports.extractGovbankData = () =>
+  new Promise((resolve, reject) => {
+    let isDone = false
+    let count = 5
+    async function recurse() {
+      count -= 1
+      dataParsers
+        .parseGovbank()
+        .then(res => {
+          if (res) {
+            const data = []
+            const findUsd = JSON.parse(res).find(cur => cur.cc === "USD")
+            data.push(findUsd)
+            const usdCourse = `NBU: ${data[0].rate}`
+            isDone = true
+            resolve(usdCourse)
+          } else if (count === 0) {
+            isDone = true
+            resolve(`Rate of currency of NBU is temporarily unavailable`)
+          }
+        })
+        .catch(err => {
+          if (!isDone) {
+            console.log(`Will retry loading Govbank data`)
+            setTimeout(recurse, 1000)
+          }
+        })
+    }
+    recurse()
+  })
